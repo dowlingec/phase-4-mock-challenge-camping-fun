4c0129bc439fc699b5777c0a5099c4308b53d598
Elizabeth Dowling <dowlinge94@gmail.com>
2022-05-24 21:16:44 -0400

    campers resources refined

--- app/models/camper.rb
+++ app/models/camper.rb
@@ -1,4 +1,8 @@
 class Camper < ApplicationRecord
     has_many :signups
     has_many :activities, through: :signups
+
+    validates :name, presence: true
+
+    validates :age, numericality: { :in 8..18 }
 end
